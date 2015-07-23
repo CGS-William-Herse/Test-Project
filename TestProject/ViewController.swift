6f92ea00028dd9dfa150948be46c996c090da8df
William Herse <William.Herse@cgs.act.edu.au>
2015-07-23 11:23:16 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by William Herse on 21/07/2015.
+//  Copyright (c) 2015 William Herse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    println("Hey Bro")
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
